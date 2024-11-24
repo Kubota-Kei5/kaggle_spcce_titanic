c43498f94ddf6409399b8f4cb15f60e96668a8af
k5 <keigo1003560@gmail.com>
2024-11-24 09:17:23 +0000

    submission

--- code/knn.py
+++ code/knn.py
@@ -4,7 +4,8 @@
 import pandas as pd
 import matplotlib.pyplot as mb
 import numpy as np
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 # %%
 train=pd.read_csv('../data/train.csv')
 test=pd.read_csv('../data/test.csv')
@@ -19,68 +20,23 @@ test.dtypes
 # %%
 test.isnull().sum()
 # %%
-x=train[['Pclass','SibSp','Parch']]
+x=train[['HomePlanet','CryoSleep','Age','VIP']]
 y=train[['Transported']]
-# %%
-from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
-# %%
-from sklearn.neighbors import KNeighborsClassifier
-
-knn=KNeighborsClassifier()
-knn.fit(x_train, y_train)
-# %%
-knn.score(x_test, y_test)
-# %%
-x_for_submit = test[['Pclass', 'SibSp', 'Parch']]
-submit = test[['PassengerId']]
-submit['Survived']=knn.predict(x_for_submit)
-
-submit
-# %%
-submit.to_csv('../submission/submit01_knn.csv', index=False)
-
-
-
+x['HomePlanet']=x['HomePlanet'].fillna("hp_unknown")
+x['CryoSleep']=x['CryoSleep'].fillna("cs_unknown")
+x['Age']=x['Age'].fillna(x['Age'].mean())
+x['VIP']=x['VIP'].fillna("vip_unknown")
 
 # %%
-#Feature Improvement and Feature Selection
-# %%
-#Feature addition
-# %%
-x=train[['Pclass','SibSp','Parch','Sex']]
-y=train[['Survived']]
-# %%
-x=pd.get_dummies(x, dtype=int, columns=['Pclass', 'Sex'])
-x
-# %%
+x=pd.get_dummies(x, dtype=int, columns=['HomePlanet', 'CryoSleep','VIP'])
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
 
-knn=KNeighborsClassifier()
-knn.fit(x_train, y_train)
+x_train
 
-knn.score(x_test, y_test)
-# %%
-#Feature addition and removing null
-# %%
-x=train[['Pclass','SibSp','Parch','Sex','Fare']]
-y=train[['Survived']]
-# %%
-x=pd.get_dummies(x, dtype=int, columns=['Pclass', 'Sex'])
-x
-# %%
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
-
-knn=KNeighborsClassifier()
-knn.fit(x_train, y_train)
-
-knn.score(x_test, y_test)
-# %%
-#Standardization
 # %%
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.neighbors import KNeighborsClassifier
 scaler=StandardScaler()
 scaler.fit(x_train)
 
@@ -92,12 +48,15 @@ knn.fit(x_train_scaled, y_train)
 
 knn.score(x_test_scaled, y_test)
 # %%
-x_for_submit = test[['Pclass', 'SibSp', 'Parch', 'Sex', 'Fare']]
+x_for_submit = test[['HomePlanet','CryoSleep','Age','VIP']]
 submit = test[['PassengerId']]
 
-x_for_submit=pd.get_dummies(x_for_submit, columns=['Pclass', 'Sex'])
+x_for_submit['HomePlanet']=x_for_submit['HomePlanet'].fillna("hp_unknown")
+x_for_submit['CryoSleep']=x_for_submit['CryoSleep'].fillna("cs_unknown")
+x_for_submit['Age']=x_for_submit['Age'].fillna(x_for_submit['Age'].mean())
+x_for_submit['VIP']=x_for_submit['VIP'].fillna("vip_unknown")
 
-x_for_submit['Fare']=x_for_submit['Fare'].fillna(x_for_submit['Fare'].mean())
+x_for_submit=pd.get_dummies(x_for_submit, dtype=int, columns=['HomePlanet', 'CryoSleep','VIP'])
 
 #scaler.fitはtrainに対してfitさせたものをtestに使うのがポイント
 #ただし、今回の場合はtestにfitさせたものでpredictしたほうがスコアは良かった
@@ -106,8 +65,8 @@ scaler.fit(x_train)
 
 x_for_submit_scaled=scaler.transform(x_for_submit)
 
-submit['Survived']=knn.predict(x_for_submit_scaled)
+submit['Transported']=knn.predict(x_for_submit_scaled)
 submit
 # %%
-submit.to_csv('../submission/submit02_knn.csv', index=False)
+submit.to_csv('../submission/submit01_knn.csv', index=False)
 # %%
